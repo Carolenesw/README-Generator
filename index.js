72fd0112e041ce3544ca49c91b6d305952692dbc
Carolene Wilson <carolenesw@gmail.com>
2020-06-17 15:03:10 -0400

    used prompt tp get user name and console log data

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
+// npm packages
 const fs = require("fs");
 const axios = require("axios");
 const inquirer = require("inquirer");
 
+// prompt user for required github information
 inquirer
-  .prompt({
-    message: "Enter your GitHub username",
-    name: "username"
-  })
-  .then(function({ username }) {
-    const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
-  });
+    .prompt([
+        {
+            type: "input",
+            message: "Enter your GitHub username",
+            name: "username"
+        }
+    ])
+
+    // use response to get url data 
+    .then(res => {
+        // const queryUrl = `https://api.github.com/users/${username}`;
+
+        if (res) {
+            console.log("Success!");
+            // this shows all information contain in the array/object 
+            console.log(res)
+          } else if (res.username == "")
+          {
+            console.log("You forgot to enter your User Name!");
+          }
+        })
