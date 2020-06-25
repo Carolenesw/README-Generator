0abe20ac6e53f0da5b265bd09bf7b5a718697450
Carolene Wilson <carolenesw@gmail.com>
2020-06-25 13:37:54 -0400

    edit badge icon

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,17 +1,18 @@
 const res = require('./index')
 const axios = require("axios");
  
-console.log("avatar: ", res.avatar_url)
+console.log("avatarUrl within generate markDown: ", res.avatar_url)
 // create function to generate answers based on user response
 // console.log(generateMarkdown(res))
-function generateMarkdown(res) {
-    console.log("prompt response", res)
-   
-    // data.license= 
-    return `
+ function generateMarkdown(res) {
+    // console.log("prompt response", res)
+    return`
 # Project Title: ${res.title}
+
 ## Badge: 
-*
+
+<img alt="NPM" src="https://img.shields.io/npm/l/inquirer?logo=github&style=flat-square"> <img alt="GitHub All Releases" src="https://img.shields.io/github/downloads/carolenesw/README.md/total?logo=Github&logoColor=%23ff0000&style=flat-square">
+
 ## Description: 
 * ${res.description}
 ## Table of Content: 
@@ -35,8 +36,8 @@ function generateMarkdown(res) {
 * ${res.testing}
 ## Questions
 If you have any questions, please contact the GitHub user.           
-<img src="${res.data.avatar_url}">  
-Email: ${res.data.name}
+<img src="${res.picture}">  
+Email: ${res.email}
 
 `;
 
